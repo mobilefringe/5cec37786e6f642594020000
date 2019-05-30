b0b6e4e2e125ba2f992c2901f6cd6bde25831b8d
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-05-30 12:28:40 -0400

    The Marketplace at The Enclave 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(196, 159, 79, 0.8), rgba(213, 179, 105, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(196, 159, 79)' }">
+                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(165, 78, 32, 0.8), rgba(165, 78, 32, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(165, 78, 32)' }">
                     <div class="main_container">
                         <h2 v-if="currentPage.title" v-html="currentPage.title"></h2>
                     </div>
