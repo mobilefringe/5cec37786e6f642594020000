f3a168eb780bbfbdf30378f2b4d7051c056ea772
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-08 14:40:28 -0400

    The Marketplace at The Enclave 1.0  /main.js

--- main.js
+++ main.js
@@ -35,7 +35,8 @@ require.config({
         'vee-validate' : 'https://cdn.jsdelivr.net/npm/vee-validate@2.1.4/dist/vee-validate',
         'vue-select': 'https://cdn.jsdelivr.net/npm/vue-select@2.3.0/dist/vue-select.min',
         'bootstrap-vue' : 'https://codecloud.cdn.speedyrails.net/sites/59347e776e6f64538f150000/text/javascript/1554736414383/bootstrap-vue',
-        'loader': 'https://mmvue.codecloudapp.com/loader.vue?noext'
+        'loader': 'https://mmvue.codecloudapp.com/loader.vue?noext',
+        'campaignMonitor': 'https://js.createsend1.com/javascript/copypastesubscribeformlogic',
     }
 });
 
